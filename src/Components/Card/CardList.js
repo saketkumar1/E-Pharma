482579abc40f249d11ab863b34bb6dba24006e7c
Kumar_Saket <saketkumar8385@gmail.com>
2023-05-04 12:36:50 +0530

    detail page

--- src/Components/Card/CardList.js
+++ src/Components/Card/CardList.js
@@ -1,11 +1,18 @@
 import React from "react";
 import Card from "./Card";
+import Detail from "../Detail/Detail";
+import { BrowserRouter as Router, Route, Link, Routes } from "react-router-dom";
 
 export default function CardList(props) {
-  return props.CardData.map((CardData) => {
+  return props.CardData.map((CardData,i) => {
     return (
       <div>
-        <Card CardData={CardData} addCart={props.addCart} />
+        <Card
+          CardData={CardData}
+          key={i}
+          index={i}
+          addCart={props.addCart}
+        ></Card>
       </div>
     );
   });
